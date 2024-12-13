anonymousx97/pyrogram

commit 7c6d90388e70cac6c00014b7f38c1c9d94d54c95

Update Types, according to TDLib

pyrogram/types/business/successful_payment.py
@@ -16,6 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime
 from typing import Union
 
 import pyrogram
@@ -37,6 +38,15 @@ class SuccessfulPayment(Object):
         invoice_payload (``str``):
             Bot specified invoice payload. Only available to the bot that received the payment.
 
+        subscription_expiration_date (:py:obj:`~datetime.datetime`, *optional*):
+            Expiration date of the subscription, in Unix time; for recurring payments only.
+
+        is_recurring (``bool``, *optional*):
+            True, if the payment is a recurring payment for a subscription.
+
+        is_first_recurring (``bool``, *optional*):
+            True, if the payment is the first payment for a subscription.
+
         shipping_option_id (``str``, *optional*):
             Identifier of the shipping option chosen by the user. Only available to the bot that received the payment.
 
@@ -49,12 +59,6 @@ class SuccessfulPayment(Object):
         provider_payment_charge_id (``str``):
             Provider payment identifier. Only available to the bot that received the payment.
 
-        is_recurring (``bool``, *optional*):
-            True, if this is a recurring payment.
-
-        is_first_recurring (``bool``, *optional*):
-            True, if this is the first recurring payment.
-
         invoice_name (``str``, *optional*):
             Name of the invoice; may be empty if unknown
 
@@ -66,12 +70,13 @@ class SuccessfulPayment(Object):
         currency: str,
         total_amount: str,
         invoice_payload: str,
-        telegram_payment_charge_id: str,
-        provider_payment_charge_id: str,
-        shipping_option_id: str = None,
-        order_info: "types.OrderInfo" = None,
+        subscription_expiration_date: datetime = None,
         is_recurring: bool = None,
         is_first_recurring: bool = None,
+        shipping_option_id: str = None,
+        order_info: "types.OrderInfo" = None,
+        telegram_payment_charge_id: str,
+        provider_payment_charge_id: str,
         invoice_name: str = None
     ):
         super().__init__()
@@ -79,12 +84,13 @@ class SuccessfulPayment(Object):
         self.currency = currency
         self.total_amount = total_amount
         self.invoice_payload = invoice_payload
-        self.telegram_payment_charge_id = telegram_payment_charge_id
-        self.provider_payment_charge_id = provider_payment_charge_id
-        self.shipping_option_id = shipping_option_id
-        self.order_info = order_info
+        self.subscription_expiration_date = subscription_expiration_date
         self.is_recurring = is_recurring
         self.is_first_recurring = is_first_recurring
+        self.shipping_option_id = shipping_option_id
+        self.order_info = order_info
+        self.telegram_payment_charge_id = telegram_payment_charge_id
+        self.provider_payment_charge_id = provider_payment_charge_id
         self.invoice_name = invoice_name
 
     @staticmethod
@@ -139,5 +145,6 @@ class SuccessfulPayment(Object):
             order_info=order_info,
             is_recurring=getattr(successful_payment, "recurring_used", None),
             is_first_recurring=getattr(successful_payment, "recurring_init", None),
-            invoice_name=getattr(successful_payment, "invoice_slug", None)
+            invoice_name=getattr(successful_payment, "invoice_slug", None),
+            subscription_expiration_date=utils.timestamp_to_datetime(successful_payment.subscription_until_date),
         )

pyrogram/methods/business/create_invoice_link.py
@@ -17,6 +17,7 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+from datetime import datetime
 from typing import List, Optional, Union
 
 import pyrogram
@@ -34,6 +35,7 @@ class CreateInvoiceLink:
         currency: str,
         prices: List["types.LabeledPrice"],
         provider_token: str = None,
+        subscription_period: datetime = None,
         max_tip_amount: int = None,
         suggested_tip_amounts: List[int] = None,
         start_parameter: str = None,
@@ -72,6 +74,9 @@ class CreateInvoiceLink:
 
             provider_token (``str``, *optional*):
                 Payment provider token, obtained via `@BotFather <https://t.me/botfather>`_. Pass an empty string for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            subscription_period (:py:obj:`~datetime.datetime`, *optional*):
+                The number of seconds the subscription will be active for before the next payment. The currency must be set to “XTR” (Telegram Stars) if the parameter is used. Currently, it must always be 2592000 (30 days) if specified.
 
             max_tip_amount (``int``, *optional*):
                 The maximum accepted amount for tips in the smallest units of the currency (integer, **not** float/double). For example, for a maximum tip of ``US$ 1.45`` pass ``max_tip_amount = 145``. See the exp parameter in `currencies.json <https://core.telegram.org/bots/payments/currencies.json>`_, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies). Defaults to 0. Not supported for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
@@ -148,7 +153,8 @@ class CreateInvoiceLink:
                     shipping_address_requested=need_shipping_address,
                     flexible=is_flexible,
                     phone_to_provider=send_phone_number_to_provider,
-                    email_to_provider=send_email_to_provider
+                    email_to_provider=send_email_to_provider,
+                    subscription_period=utils.datetime_to_timestamp(subscription_period)
                 ),
                 payload=payload.encode() if isinstance(payload, str) else payload,
                 provider=provider_token,

pyrogram/methods/business/get_available_gifts.py
@@ -27,7 +27,7 @@ class GetAvailableGifts:
     ) -> List["types.Gift"]:
         """Get all gifts that can be sent to other users.
 
-        .. include:: /_includes/usable-by/users.rst
+        .. include:: /_includes/usable-by/users-bots.rst
 
         Returns:
             List of :obj:`~pyrogram.types.Gift`: On success, a list of star gifts is returned.

pyrogram/methods/business/get_business_connection.py
@@ -16,7 +16,6 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime
 from typing import Union, List
 
 import pyrogram

pyrogram/methods/business/send_gift.py
@@ -44,10 +44,10 @@ class SendGift:
                 For a contact that exists in your Telegram address book you can use his phone number (str).
 
             gift_id (``int``):
-                Unique identifier of the gift to send.
+                Identifier of the gift.
 
             text (``str``, *optional*):
-                Text of the message to be sent. 0-``gift_text_length_max`` characters.
+                Text that will be shown along with the gift. 0-``gift_text_length_max`` characters.
 
             parse_mode (:obj:`~pyrogram.enums.ParseMode`, *optional*):
                 By default, texts are parsed using both Markdown and HTML styles.
@@ -58,7 +58,7 @@ class SendGift:
                 Only Bold, Italic, Underline, Strikethrough, Spoiler, and CustomEmoji entities are allowed.
 
             is_private (``bool``, *optional*):
-                Pass True to show the current user as sender and gift text only to the gift receiver; otherwise, everyone will be able to see them.
+                For users only: Pass True to show the current user as sender and gift text only to the gift receiver; otherwise, everyone will be able to see them.
 
         Returns:
             ``bool``: On success, True is returned.

pyrogram/methods/business/send_invoice.py
@@ -91,7 +91,7 @@ class SendInvoice:
                 Price breakdown, a JSON-serialized list of components (e.g. product price, tax, discount, delivery cost, delivery tax, bonus, etc.). Must contain exactly one item for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
 
             message_thread_id (``int``, *optional*):
-                If the message is in a thread, ID of the original message.
+                Unique identifier for the target message thread (topic) of the forum; for forum supergroups only.
 
             provider_token (``str``, *optional*):
                 Payment provider token, obtained via `@BotFather <https://t.me/botfather>`_. Pass an empty string for payments in `Telegram Stars <https://t.me/BotNews/90>`_.

pyrogram/methods/chat_topics/close_forum_topic.py
@@ -16,8 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime
-from typing import Union, List
+from typing import Union
 
 import pyrogram
 from pyrogram import types, utils, raw

pyrogram/methods/chat_topics/create_forum_topic.py
@@ -16,8 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime
-from typing import Union, List
+from typing import Union
 
 import pyrogram
 from pyrogram import types, utils, raw

commit 5b29cd9d210205cb2e065a1f4cdddb409c89ce26

(fix): Missing Import statement
Closes #122

Regression was introduced in 7c6d903

pyrogram/types/business/successful_payment.py
@@ -20,7 +20,7 @@ from datetime import datetime
 from typing import Union
 
 import pyrogram
-from pyrogram import raw, types
+from pyrogram import raw, types, utils
 
 from ..object import Object
 